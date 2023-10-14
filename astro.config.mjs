845845ac5535622b8e162fb4ba735e3eeb2598f6
Eduard Pochtar <4650029+eduarddotgg@users.noreply.github.com>
2023-10-14 20:50:08 +0200

    fix: react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
 	site: 'https://constitution.hu',
-	integrations: [mdx(),  tailwind(), react()],
+	integrations: [mdx(),  tailwind(), react({
+		experimentalReactChildren: true,
+	})],
 });

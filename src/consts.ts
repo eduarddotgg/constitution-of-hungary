d9bccef7ad34910e9cd8911591cc6404541fb378
Eduard Pochtar <4650029+eduarddotgg@users.noreply.github.com>
2023-10-16 08:57:48 +0200

    fix: lighthouse updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'Magyarország Alaptörvénye';
+export const SITE_TITLE = 'Magyarország Alaptörvénye (2011. április 25.)';
 export const SITE_DESCRIPTION = 'Magyarország Alaptörvénye fejezetei és cikkelyei kényelmes formában. Tervezte: Pocstár Eduárd';

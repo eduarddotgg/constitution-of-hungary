3c776a88a9d0e01c5c50217f1b3a236e6a8e6bc7
Eduard Pochtar <4650029+eduarddotgg@users.noreply.github.com>
2023-10-16 08:56:32 +0200

    fix: lighthouse updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Magyarország Alaptörvénye';
-export const SITE_DESCRIPTION = '';
+export const SITE_DESCRIPTION = 'Magyarország Alaptörvénye fejezetei és cikkelyei kényelmes formában. Tervezte: Pocstár Eduárd';
